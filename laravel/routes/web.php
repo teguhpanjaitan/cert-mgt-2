1d091f3e1cd3c72e8ce2b668dbe2f04ca5808bf3
Teguh <teguhpanjaitan@gmail.com>
2020-03-24 16:54:19 +0700

    Fix route

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -34,10 +34,10 @@ Route::get('/login', 'AuthenticationController@showLogin')->name("login");
 Route::post('/login', 'AuthenticationController@doLogin');
 
 // Password Reset Routes...
-Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
-Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+Route::get('/password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('/password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('/password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('/password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
 
 Route::get('/home', function () {
     return redirect('/');
