2be0b02d266339eab62b76a94a54eaf28c232069
Teguh <teguhpanjaitan@gmail.com>
2020-03-30 08:24:02 +0700

    Add academic table

---
+++ laravel/database/migrations/2020_03_30_090838_create_academics_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAcademicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('academics', function (Blueprint $table) {
+            $table->id();
+            $table->string("number",25);
+            $table->string("name",250);
+            $table->date("date");
+            $table->string("type",100);
+            $table->string("awarded",250);
+            $table->string("certified",250);
+            $table->unsignedBigInteger("updated_by");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('academics');
+    }
+}
