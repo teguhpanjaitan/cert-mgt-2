9a638f7290663a733a95f8c1c0ee3fa56e25f8a6
Teguh <teguhpanjaitan@gmail.com>
2020-03-30 16:15:28 +0700

    Just except one route for CSRF instead of use API

--- laravel/app/Http/Controllers/FrontendCertController.php
+++ laravel/app/Http/Controllers/FrontendCertController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Academic as Certificate;
-use \Carbon\Carbon;
 
 class FrontendCertController extends Controller
 {
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -17,7 +17,3 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
-
-Route::get('/frontend/academiccertificatechecker','FrontendCertController@index');
-Route::get('/frontend/academiccertificatecheckerresultpage','FrontendCertController@result');
-Route::post('/academic/ajax','FrontendCertController@find');
